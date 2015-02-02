1ed19b77b8d0b32424fcb1cfd60681b17940e866
zkornbluth <zk49844@students.westport.k12.ct.us>
2015-02-02 18:34:05 -0500

    different images

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,7 +3,7 @@ Bundler.require
 
 get '/roll/' do
   num = [1, 2, 3, 4, 5, 6].sample.to_s
-  send_file File.join(settings.public_folder, "/#{num}.jpg")
+  send_file File.join(settings.public_folder, "/die_face_#{num}.jpg")
 end
 
 get '/roll/:num' do
@@ -13,6 +13,6 @@ get '/roll/:num' do
     rolled.push([1, 2, 3, 4, 5, 6].sample)
   end
   rolled.each do |num|
-    send_file File.join(settings.public_folder, "/#{num}.jpg")
+    send_file File.join(settings.public_folder, "/die_face_#{num}.jpg")
   end
 end
